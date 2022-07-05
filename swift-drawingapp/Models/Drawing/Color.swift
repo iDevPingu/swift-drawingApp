36fefeb128c2e12005582bc371907330eb3cbddc
idev.pingu <idev.pingu@gmail.com>
2022-07-05 17:58:28 +0900

    [Step-3] RandomColorGenerator 작업

--- swift-drawingapp/Models/Drawing/Color.swift
+++ swift-drawingapp/Models/Drawing/Color.swift
@@ -12,3 +12,15 @@ protocol RandomValueGenerator {
     associatedtype RandomValue
     static func random() -> RandomValue
 }
+
+final class RandomColorGenerator: RandomValueGenerator {
+    typealias RandomValue = UIColor
+    
+    static func random() -> UIColor {
+        let randomRed = CGFloat((1...255).randomElement() ?? 255)
+        let randomGreen = CGFloat((1...255).randomElement() ?? 255)
+        let randomBlue = CGFloat((1...255).randomElement() ?? 255)
+        
+        return UIColor(red: randomRed, green: randomGreen, blue: randomBlue, alpha: 1)
+    }
+}
