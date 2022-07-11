f05a521e6b297cb5b41880aa71e38d4d93286dee
idev.pingu <idev.pingu@gmail.com>
2022-07-12 01:10:27 +0900

    [Step3] Rectangle 그리기 로직과 View 연결 작업

--- swift-drawingapp/View/Sketchable/SketchView.swift
+++ swift-drawingapp/View/Sketchable/SketchView.swift
@@ -12,17 +12,47 @@ final class SketchView: UIView, Sketchable, DrawCommander {
     var drawer: Drawer
     var shapes: [Shape] = []
     
+    private lazy var buttonStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [drawRectangleButton])
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 0.0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private lazy var drawRectangleButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(drawRectangle), for: .touchUpInside)
+        button.setTitle("사각형", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
     init() {
         drawer = ShapeDrawer()
         super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .white
+        
+        addSubview(buttonStackView)
+        
+        NSLayoutConstraint.activate([
+            buttonStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            buttonStackView.heightAnchor.constraint(equalToConstant: 60),
+            buttonStackView.widthAnchor.constraint(equalToConstant: 150),
+            buttonStackView.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @objc
     func drawRectangle() {
         let rectangle = RandomRectangleFactory.randomShape(at: self)
+        shapes.append(rectangle)
         drawer.draw(at: self, with: rectangle)
     }
 }
--- swift-drawingapp/ViewController.swift
+++ swift-drawingapp/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let sketchView = SketchView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(sketchView)
+        
+        NSLayoutConstraint.activate([
+            sketchView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            sketchView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            sketchView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            sketchView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+        ])
     }
 
 
